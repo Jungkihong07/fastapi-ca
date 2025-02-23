587f18dc057f326d10e4abb8e8fff7817c3d302c
Jungkihong07 <hnn07185@gmail.com>
2025-02-24 01:22:35 +0900

    페이지네이션 기능 구현 완료

--- user/infra/repository/user_repo.py
+++ user/infra/repository/user_repo.py
@@ -59,8 +59,14 @@ class UserRepository(IUserRepository):
 
         return user
 
-    def get_users(self) -> list[UserVO]:
+    def get_users(
+        self, page: int = 1, items_per_page: int = 10
+    ) -> tuple[int, list[UserVO]]:
         with SessionLocal() as db:
-            users = db.query(User).all()
+            query = db.query(User)
+            total_count = query.count()
 
-        return [map_user(user) for user in users]
+            offset = (page - 1) * items_per_page
+            users = query.limit(items_per_page).offset(offset).all()
+
+        return total_count, [map_user(user) for user in users]
