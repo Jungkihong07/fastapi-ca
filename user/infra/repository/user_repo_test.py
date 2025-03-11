1a16ed02b83fb257054c2dd2b66255d8ee298ec7
Jungkihong07 <hnn07185@gmail.com>
2025-03-11 23:58:22 +0900

    User Repository test 코드 수정.

--- user/infra/repository/user_repo_test.py
+++ user/infra/repository/user_repo_test.py
@@ -8,6 +8,7 @@ from user.infra.repository.user_repo import UserRepository
 from utils.db_utils import map_user
 
 
+# 해당 테스트 코드는 SQLALchmey의 세션을 모의한 픽스처를 제공한다.
 @pytest.fixture
 def mock_session_local():
     with patch(
@@ -19,8 +20,11 @@ def mock_session_local():
 def test_find_by_email_user_exists(mock_session_local):
     mock_user = User(id=1, email="test@example.com", name="Test User")
     mock_db = Mock()
+    # 유저 저장소가 검색결과로 반환되는 유저 도메인 객체를 모의힌다.
     mock_db.query.return_value.filter.return_value.first.return_value = mock_user
+    # 생성되는 세션을 모의힌다.
     mock_session_local.return_value.__enter__.return_value = mock_db
+
     user_repository = UserRepository()
 
     result = user_repository.find_by_email("test@example.com")
@@ -30,10 +34,12 @@ def test_find_by_email_user_exists(mock_session_local):
 
 def test_find_by_email_user_does_not_exist(mock_session_local):
     mock_db = Mock()
+    # 유저 저장소의 검색 결과를 None으로 설정한다.
     mock_db.query.return_value.filter.return_value.first.return_value = None
     mock_session_local.return_value.__enter__.return_value = mock_db
     user_repository = UserRepository()
 
+    # 예외가 일어났는지 확인한다.
     with pytest.raises(HTTPException) as exception:
         user_repository.find_by_email("nonexistent@exmple.com")
 
